b88c043c95af84010c567b5e20b9fb94bf598356
313392073 <313392073@qq.com>
2019-02-27 17:26:49 +0800

    新增首页跳转

--- routes/route/index.js
+++ routes/route/index.js
@@ -2,15 +2,6 @@ const common = require('../interface');
 const async = require('async');
 module.exports = function (router) {
   router.get('/', function (req, res) {
-    common.getToken(req, function () {
-      async.auto({
-        res1: function (done) {
-          var obj = {};
-          common.test(req, obj, done);
-        },
-      }, function (err, result) {
-        res.render('index', { title: '首页' })
-      })
-    })
+    res.redirect('/login.html')
   })
 }

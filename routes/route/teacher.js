2d1818a871c1e7b5a82fa40a3b7ac85d6cce244d
x xiao <x xiao>
2019-04-29 20:06:35 +0800

    提交

--- routes/route/teacher.js
+++ routes/route/teacher.js
@@ -104,7 +104,7 @@ module.exports = function(router){
                api.teaClassList(req,params,done)
             }]
         },function(error,result) {
-            console.log(result['res2']['obj'])
+            console.log(result['res2'])
            res.render('tchCorrectRate',{
                title:'老师课前习题正确率',
                trData:result['res2']?result['res2']['obj']:[],
@@ -209,8 +209,7 @@ module.exports = function(router){
                api.teaGroupData(req,params,done)
             }]
         },function(error,result) {
-            console.log(result['res2']['obj'])
-            console.log(result['res2']['obj'][0]['comments'])
+            // console.log(result['res2']['obj'])
            res.render('tchPanelData',{
                title:'老师小组数据',
                trData:result['res2']?result['res2']['obj']:[],
